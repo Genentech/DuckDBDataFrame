e0324474831ec2a28395954e658a19de518a80bc
Patrick Aboyoun <aboyoun.patrick@gene.com>
2026-08-27 14:32:13 -0700

    fix: the previous fix

--- R/DuckDBTable-class.R
+++ R/DuckDBTable-class.R
@@ -474,11 +474,16 @@ setMethod("dbconn", "DuckDBTable", function(x) x@conn$src$con)
         if (i %in% names(dimtbls)) {
             # Filter with dimension tables
             dimtbl <- dimtbls[[i]]
-            comp <- setdiff(rownames(dimtbl) %||% seq_len(nrow(dimtbl)), set)
+            dimtbl_rn <- rownames(dimtbl)
+            comp <- setdiff(dimtbl_rn %||% seq_len(nrow(dimtbl)), set)
             ncomp <- length(comp)
             if (ncomp > 0L) {
                 # Add the dimension table filters
-                sectbl <- dimtbl[as.character(set), , drop = FALSE]
+                sectbl <- if (is.null(dimtbl_rn)) {
+                    dimtbl[set, , drop = FALSE]
+                } else {
+                    dimtbl[as.character(set), , drop = FALSE]
+                }
                 for (j in colnames(sectbl)) {
                     part <- unique(sectbl[[j]])
                     comp2 <- setdiff(unique(dimtbl[[j]]), part)
